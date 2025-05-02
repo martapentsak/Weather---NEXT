a35599eff7576d80ef3438dadbdb0b70bc4feca0
Peter Pentsak <pencak.petro@gmail.com>
2025-05-02 11:29:40 +0300

    Formatted code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
-
 import { LocationSelector } from "./components/LocationSelector";
 import { LocationProvider } from "./context/location";
 
 export default function Home() {
   return (
-   <main>
-    <LocationProvider>
-    <LocationSelector/>
-    </LocationProvider>
+    <main>
+      <LocationProvider>
+        <LocationSelector />
+      </LocationProvider>
     </main>
   );
 }
