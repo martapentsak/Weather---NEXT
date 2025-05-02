dc651fe39e91914e3ab01b622c594f192d91bbef
Peter Pentsak <pencak.petro@gmail.com>
2025-05-02 11:36:45 +0300

    Formatted code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,12 @@
-
-import { LocationSelector } from "./components/LocationSelector";
-import { WeatherCard } from "./components/WeatherCard";
 import { LocationProvider } from "./context/location";
-import QueryBuilderIcon from '@mui/icons-material/QueryBuilder';
 import { HomePage } from "./pages/HomePage";
 
 export default function Home() {
   return (
-   <div>
-    <LocationProvider>
-<HomePage/>
-    </LocationProvider>
+    <div>
+      <LocationProvider>
+        <HomePage />
+      </LocationProvider>
     </div>
   );
 }
