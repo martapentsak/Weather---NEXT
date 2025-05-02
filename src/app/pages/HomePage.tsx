88d9f159a3b00b2e45df96a84fefdc8edbd5cd1a
Peter Pentsak <pencak.petro@gmail.com>
2025-05-02 11:37:18 +0300

    Changed css

--- src/app/pages/HomePage.tsx
+++ src/app/pages/HomePage.tsx
@@ -1,9 +1,10 @@
+"use client";
 import { LocationSelector } from "../components/LocationSelector";
 
 export const HomePage = () => {
   return (
-    <div className="relative w-full h-screen">
-      <main className="w-[85%] flex flex-col items-center justify-center p-[10px]">
+    <div className="relative w-full h-screen flex items-center justify-center">
+      <main className="w-[60%] h-[90%] flex flex-col items-center  p-[10px]">
         <div className="w-full flex items-center justify-end">
           <LocationSelector />
         </div>
