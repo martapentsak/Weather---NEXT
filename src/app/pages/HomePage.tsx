d6ae7015a2e9d4631e0f1086ef9370fdcccb0860
Peter Pentsak <pencak.petro@gmail.com>
2025-05-02 18:22:21 +0300

    Formatted code

--- src/app/pages/HomePage.tsx
+++ src/app/pages/HomePage.tsx
@@ -4,19 +4,17 @@ import { LocationSelector } from "../components/LocationSelector";
 import { useLocation } from "../context/location";
 
 export const HomePage = () => {
-  const {location} = useLocation()
+  const { location } = useLocation();
 
-  if (!location) return <p>Loading</p>
+  if (!location) return <p>Loading</p>;
 
- 
   return (
     <div className="relative w-full h-screen flex items-center justify-center">
       <main className="w-[60%] h-[90%] flex flex-col items-center  p-[10px]">
         <div className="w-full flex items-center justify-end">
           <LocationSelector />
         </div>
-        <HourlyWeatherContainer location={location}/>
-
+        <HourlyWeatherContainer location={location} />
       </main>
     </div>
   );
