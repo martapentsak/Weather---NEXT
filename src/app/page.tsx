4ba1fb714ff210bbc3da306479143926af5efc59
Peter Pentsak <pencak.petro@gmail.com>
2025-05-12 15:17:31 +0300

    Deleted LocationProvider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 import { LocationSelector } from "./components/LocationSelector";
-import { LocationProvider } from "./context/location";
 
 export default function Home() {
   return (
     <main>
-      <LocationProvider>
-        <LocationSelector />
-      </LocationProvider>
+      <LocationSelector />
     </main>
   );
 }
