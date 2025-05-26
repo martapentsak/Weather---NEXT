4131750e6b89d855fe983f4852db38b8b2cf9d7b
Peter Pentsak <pencak.petro@gmail.com>
2025-05-26 12:34:10 +0300

    Added updates from prev branch (LocationSelector)

--- src/app/components/LocationSelector/index.tsx
+++ src/app/components/LocationSelector/index.tsx
@@ -1,30 +1,32 @@
 "use client";
 
-import axios from "axios";
 import { useEffect, useState } from "react";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
 
 import cities from "../../../../cities.json";
 import { Location } from "../../types";
+import axios from "axios";
 import { useWeather } from "@/app/context/weather";
 
 const locationStorageKey = "location";
 
 export const LocationSelector = () => {
   const [location, setLocation] = useState<Location | null>(null);
-  const [mounted, setMounted] = useState(false);
 
   const { getHourlyWeather } = useWeather();
 
   const formatCityLabel = (location: Location) =>
     `${location.city}, ${location.country}`;
 
+  const uniqueCities = Array.from(
+    new Map(cities.map((c) => [`${c.city},${c.country}`, c])).values()
+  );
+
   const handleChangeSelectedCity = async (value: string | null) => {
     if (!value) return;
     const cityObj = cities.find((v) => value.includes(v.city));
     if (!cityObj) return;
-
     const { city, country, lat, lng } = cityObj;
     const newLocation = {
       city,
@@ -33,8 +35,8 @@ export const LocationSelector = () => {
       longitude: lng,
     };
     setLocation(newLocation);
-    localStorage.setItem(locationStorageKey, JSON.stringify(newLocation));
     getHourlyWeather(newLocation);
+    localStorage.setItem(locationStorageKey, JSON.stringify(newLocation));
   };
 
   const getLocationFromCoordinates = async (
@@ -42,10 +44,8 @@ export const LocationSelector = () => {
     longitude: number
   ) => {
     try {
-      const { name, country, lat, lon } = await fetchLocationFromCoords(
-        latitude,
-        longitude
-      );
+      const res = await axios(`/api/location?lat=${latitude}&lon=${longitude}`);
+      const { name, country, lat, lon } = await res.data;
       const locationFromCoordinates = {
         city: name,
         country,
@@ -57,21 +57,19 @@ export const LocationSelector = () => {
         locationStorageKey,
         JSON.stringify(locationFromCoordinates)
       );
-      getHourlyWeather(locationFromCoordinates);
     } catch (error) {
       console.error("Failed to fetch city", error);
     }
   };
 
   useEffect(() => {
-    setMounted(true);
-
     const initLocation = () => {
       const storedLocation = localStorage.getItem(locationStorageKey);
       if (storedLocation) {
         const parsedStoredLocation = JSON.parse(storedLocation);
         setLocation(parsedStoredLocation);
         getHourlyWeather(parsedStoredLocation);
+
         return;
       }
 
@@ -79,7 +77,6 @@ export const LocationSelector = () => {
         console.warn("Geolocation not supported");
         return;
       }
-
       navigator.geolocation.getCurrentPosition((pos) => {
         getLocationFromCoordinates(pos.coords.latitude, pos.coords.longitude);
       });
@@ -88,15 +85,13 @@ export const LocationSelector = () => {
     initLocation();
   }, []);
 
-  if (!mounted) return null;
-
   return (
     <Autocomplete
       value={location ? formatCityLabel(location) : "Detecting location..."}
       onChange={(_event: any, newValue: string | null) => {
         handleChangeSelectedCity(newValue);
       }}
-      options={cities.map((city) => formatCityLabel(city))}
+      options={uniqueCities.map((city) => formatCityLabel(city))}
       sx={{ minWidth: 200, width: 300 }}
       renderInput={(params) => (
         <TextField {...params} sx={{ padding: "1px" }} />
@@ -104,10 +99,3 @@ export const LocationSelector = () => {
     />
   );
 };
-
-async function fetchLocationFromCoords(latitude: number, longitude: number) {
-  const response = await axios.get(
-    `https://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${process.env.NEXT_PUBLIC_LOCATION_API}`
-  );
-  return response.data[0];
-}
