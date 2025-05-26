7664933f48bea9673b79fb88d691d4da318aa036
Peter Pentsak <pencak.petro@gmail.com>
2025-05-26 12:44:36 +0300

    Added component to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,23 @@
+"use client";
 import { HourlyWeatherContainer } from "./components/HourlyWeather";
 import { LocationSelector } from "./components/LocationSelector";
+import { WeeklyWeatherContainer } from "./components/WeeklyWeather";
 
 export default function Home() {
   return (
     <div className="relative w-full h-screen flex items-center justify-center">
-      <main className="w-[60%] h-[90%] flex flex-col items-center  p-[10px]">
-        <div className="w-full flex items-center justify-end">
+      <main className="w-[70%] h-[95%] flex flex-col items-center  p-[10px] absolute">
+        <div className="w-full flex items-center justify-end ">
           <LocationSelector />
         </div>
-        <HourlyWeatherContainer />
+        <div className="w-full">
+          <HourlyWeatherContainer />
+          <div className="w-full flex items-start mt-[10px] justify-between">
+            <div className="flex flex-col align-between">
+              <WeeklyWeatherContainer />
+            </div>
+          </div>
+        </div>
       </main>
     </div>
   );
