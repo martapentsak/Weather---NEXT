d00936521c23dc3977f38ea44e2212ab56640689
Peter Pentsak <pencak.petro@gmail.com>
2025-05-27 15:57:02 +0300

    Changed LocationSelector component

--- src/app/components/LocationSelector/index.tsx
+++ src/app/components/LocationSelector/index.tsx
@@ -1,83 +1,44 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
+import { useParams, useRouter } from "next/navigation";
 
 import cities from "../../../../cities.json";
-import { Location } from "../../types";
-import axios from "axios";
 
-const locationStorageKey = "location";
+type Location = {
+  city: string;
+  country: string;
+};
+
+const uniqueCities = Array.from(
+  new Map(cities.map((c) => [`${c.city},${c.country}`, c])).values()
+);
 
 export const LocationSelector = () => {
-  const [location, setLocation] = useState<Location | null>(null);
+  const params = useParams();
+  const { city, country } = uniqueCities.find(
+    (info) => info.city.toLowerCase() === params.city
+  )!;
+  const [location, setLocation] = useState<Location>({
+    city,
+    country,
+  });
+
+  const router = useRouter();
 
   const formatCityLabel = (location: Location) =>
     `${location.city}, ${location.country}`;
-
-  const uniqueCities = Array.from(
-    new Map(cities.map((c) => [`${c.city},${c.country}`, c])).values()
-  );
 
   const handleChangeSelectedCity = async (value: string | null) => {
     if (!value) return;
     const cityObj = cities.find((v) => value.includes(v.city));
     if (!cityObj) return;
-    const { city, country, lat, lng } = cityObj;
-    const newLocation = {
-      city,
-      country,
-      latitude: lat,
-      longitude: lng,
-    };
-    setLocation(newLocation);
-    localStorage.setItem(locationStorageKey, JSON.stringify(newLocation));
+    const { city, country } = cityObj;
+    setLocation({ city, country });
+    router.push(`/weather/${city.toLowerCase()}`);
   };
-
-  const getLocationFromCoordinates = async (
-    latitude: number,
-    longitude: number
-  ) => {
-    try {
-      const res = await axios(`/api/location?lat=${latitude}&lon=${longitude}`);
-      const { name, country, lat, lon } = await res.data;
-      const locationFromCoordinates = {
-        city: name,
-        country,
-        latitude: lat,
-        longitude: lon,
-      };
-      setLocation(locationFromCoordinates);
-      localStorage.setItem(
-        locationStorageKey,
-        JSON.stringify(locationFromCoordinates)
-      );
-    } catch (error) {
-      console.error("Failed to fetch city", error);
-    }
-  };
-
-  useEffect(() => {
-    const initLocation = () => {
-      const storedLocation = localStorage.getItem(locationStorageKey);
-      if (storedLocation) {
-        const parsedStoredLocation = JSON.parse(storedLocation);
-        setLocation(parsedStoredLocation);
-        return;
-      }
-
-      if (!navigator.geolocation) {
-        console.warn("Geolocation not supported");
-        return;
-      }
-      navigator.geolocation.getCurrentPosition((pos) => {
-        getLocationFromCoordinates(pos.coords.latitude, pos.coords.longitude);
-      });
-    };
-
-    initLocation();
-  }, []);
 
   return (
     <Autocomplete
